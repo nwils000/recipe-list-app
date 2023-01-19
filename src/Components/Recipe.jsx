ccea2a492b473f61cfbf6e6cdfb52236d830d309
nwils000 <nwils000@gmail.com>
2023-01-19 14:29:38 -0500

    Added button delete, add functionality + styling

--- src/Components/Recipe.jsx
+++ src/Components/Recipe.jsx
@@ -1,38 +1,48 @@
 import React from "react";
 import IngredientList from "./IngredientList";
 
-export default function Recipe(props) {
-  const { name, cookTime, servings, instructions, ingredients } = props;
+export default function Recipe({
+  id,
+  name,
+  cookTime,
+  servings,
+  instructions,
+  ingredients,
+  handleRecipeDelete,
+}) {
   return (
-    <div>
-      <div className="recipe">
-        <div>
-          <h3>{name}</h3>
-        </div>
-        <div className="recipe__row">
-          <span className="recipe__label">Cook Time:</span>
-          <span className="recipe__value">{cookTime}</span>
-        </div>
-        <div className="recipe__row">
-          <span className="recipe__label">Servings:</span>
-          <span className="recipe__value">{servings}</span>
-        </div>
-        <div className="recipe__row">
-          <span className="recipe__label">Instructions:</span>
-          <div className="recipe__value recipe__instructions recipe__value-indented">
-            {instructions}
-          </div>
-        </div>
-        <div className="recipe__row">
-          <span className="recipe__label">Ingredients:</span>
-          <div className="recipe__value recipe__value-indented">
-            <IngredientList ingredients={ingredients} />
-          </div>
+    <div className="recipe">
+      <div className="recipe__header">
+        <h3 className="recipe__title">{name}</h3>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Cook Time:</span>
+        <span className="recipe__value">{cookTime}</span>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Servings:</span>
+        <span className="recipe__value">{servings}</span>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Instructions:</span>
+        <div className="recipe__value recipe__instructions recipe__value-indented">
+          {instructions}
         </div>
       </div>
-      <div className="recipe__buttons">
+      <div className="recipe__row">
+        <span className="recipe__label">Ingredients:</span>
+        <div className="recipe__value recipe__value-indented">
+          <IngredientList ingredients={ingredients} />
+        </div>
+      </div>
+      <div>
         <button className="button button-edit">Edit</button>
-        <button className="button button-delete">Delete</button>
+        <button
+          className="button button-delete"
+          onClick={() => handleRecipeDelete(id)}
+        >
+          Delete
+        </button>
       </div>
     </div>
   );
--- src/Components/RecipeList.jsx
+++ src/Components/RecipeList.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 import Recipe from "./Recipe";
 
-export default function RecipeList({ recipes }) {
+export default function RecipeList({
+  recipes,
+  handleRecipeAdd,
+  handleRecipeDelete,
+}) {
   return (
     <div className="recipe-list">
       <div>
         {recipes.map((recipe) => {
-          return <Recipe key={recipe.id} {...recipe} />;
+          return (
+            <Recipe
+              key={recipe.id}
+              handleRecipeDelete={handleRecipeDelete}
+              {...recipe}
+            />
+          );
         })}
+      </div>
+      <div className="recipe-list__add-recipe-btn-container">
+        <button className="button button-edit" onClick={handleRecipeAdd}>
+          Add Recipe
+        </button>
       </div>
     </div>
   );
